d7e4b392ba4a0fe42a996311d78e4f4b9e2259a4
carlos33936079 <38385582+carlos33936079@users.noreply.github.com>
2018-04-14 16:50:48 -0300

    Update program.c

--- src/program.c
+++ src/program.c
@@ -1,6 +1,7 @@
 void setup(void)
 {
 	pinMode(16, OUTPUT);
+	pinMode(21, OUTPUT);
 	
 }
 
@@ -9,6 +10,12 @@ void loop(void)
 	digitalWrite(16, HIGH);
 	delay(300);
 	digitalWrite(16, LOW);
+	delay(100);
+	
+	
+	digitalWrite(21, HIGH);
+	delay(300);
+	digitalWrite(21, LOW);
 	delay(100);
 }
  
